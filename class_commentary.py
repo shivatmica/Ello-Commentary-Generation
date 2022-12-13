5f9a1ee875c4c51851aa101ff4686c434498c068
shivatmica <62459136+shivatmica@users.noreply.github.com>
2022-12-13 13:09:29 +0530

    converted instance variable (df) to class variable (self.df)

--- class_commentary.py
+++ class_commentary.py
@@ -9,6 +9,7 @@ class Preprocess:
         self.reject_names = ['9780062381828.xlsx', '9780545825030.xlsx', '9780547076690.xlsx', '9780142414538.xlsx']
         self.valid_paths_comm = []
         self.data = []
+        self.df = pd.DataFrame()
         self.main_data = pd.DataFrame()
     
     def generate_valid_paths(self):
@@ -20,27 +21,27 @@ class Preprocess:
     def generate_reject_names(self):
         for subdir in self.valid_paths_comm:
             for file in os.scandir(subdir):
-                df = pd.read_excel(file.path, usecols= list(range(1, 3)))
-                if len(df.columns) == 1:
+                self.df = pd.read_excel(file.path, usecols= list(range(1, 3)))
+                if len(self.df.columns) == 1:
                     self.reject_names.append(file.path[71:])
     
     def preprocess(self):    
         for subdir in self.valid_paths_comm:
             for file in os.scandir(subdir):
-                df = pd.read_excel(file.path, usecols= list(range(1, 3)))
+                self.df = pd.read_excel(file.path, usecols= list(range(1, 3)))
                 
                 if file.path[71:] in self.reject_names:
                     continue
 
-                if list(df.columns) != ['Text', 'Commentary']:
-                    df = df.shift(1)
-                    df.loc[0] = df.columns
-                    df.columns = ['Text', 'Commentary']
+                if list(self.df.columns) != ['Text', 'Commentary']:
+                    self.df = self.df.shift(1)
+                    self.df.loc[0] = self.df.columns
+                    self.df.columns = ['Text', 'Commentary']
 
-                df = df[~df['Commentary'].isin(['P', '-', 'Praise'])]
-                df.dropna(inplace = True)
+                self.df = self.df[~self.df['Commentary'].isin(['P', '-', 'Praise'])]
+                self.df.dropna(inplace = True)
                 
-                self.data.append(df)
+                self.data.append(self.df)
 
     def return_main_df(self):
         self.generate_valid_paths()
